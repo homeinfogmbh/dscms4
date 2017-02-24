97406aad57d061a0e05ff5da9c6dc094fde68e9c
Richard Neumann <r.neumann@homeinfo.de>
2017-02-24 10:23:25 +0100

    Updated charts

--- dscms4/orm/charts.py
+++ dscms4/orm/charts.py
@@ -17,8 +17,8 @@ class BaseChart(DSCMS4Model):
     title = CharField(255, null=True, default=None)
     text = TextField(null=True, default=None)
     created = DateTimeField()
-    begins = DateTimeField(null=True, default=None)
-    expires = DateTimeField(null=True, default=None)
+    schedule = ForeignKeyField(
+        Schedule, db_column='schedule', null=True, default=None)
 
     @property
     def active(self):
@@ -38,11 +38,8 @@ class BaseChart(DSCMS4Model):
         if self.text is not None:
             dictionary['text'] = self.text
 
-        if self.begins is not None:
-            dictionary['begins'] = self.begins.isoformat()
-
-        if self.expires is not None:
-            dictionary['expires'] = self.expires.isoformat()
+        if self.schedule is not None:
+            dictionary['schedule'] = self.schedule.to_dict()
 
         return dictionary
 
@@ -51,6 +48,17 @@ class _Chart(DSCMS4Model):
     """Abstract chart class for extension"""
 
     base_chart = ForeignKeyField(BaseChart, db_column='base_chart')
+
+    def to_dict(self):
+        """Returns a JSON compatible dictionary"""
+        return self.base_chart.to_dict()
+
+
+class LPT(_Chart):
+    """Local public transport chart"""
+
+    class Meta:
+        db_table = 'lpt_chart'
 
 
 class NewsChart(_Chart):
@@ -63,28 +71,10 @@ class NewsChart(_Chart):
 
     def to_dict(self):
         """Returns a JSON compatible dictionary"""
-        dictionary = self.base_chart.to_dict()
+        dictionary = super().to_dict()
 
         if self.localization is not None:
             dictionary['localization'] = self.localization
-
-        return dictionary
-
-
-class WeatherChart(_Chart):
-    """Weather data"""
-
-    class Meta:
-        db_table = 'weather_chart'
-
-    region = CharField(255)
-
-    def to_dict(self):
-        """Returns a JSON compatible dictionary"""
-        dictionary = self.base_chart.to_dict()
-
-        if self.region is not None:
-            dictionary['region'] = self.region
 
         return dictionary
 
@@ -95,28 +85,20 @@ class QuotesChart(_Chart):
     class Meta:
         db_table = 'quotes_chart'
 
-    def to_dict(self):
+class VideoChart(_Chart):
+    """A chart that may contain images and texts"""
+
+    class Meta:
+        db_table = 'video_chart'
+
+    file = IntegerField()
+    video = FileProperty(file)
+
+    def to_dict(self, file_name=None):
         """Returns a JSON compatible dictionary"""
-        return self.base_chart.to_dict()
-
-
-class GuessPicture(_Chart):
-    """Chart for guessing pictures"""
-
-    class Meta:
-        db_table = 'quotes_chart'
-
-
-class FacebookChart(_Chart):
-    """Facebook data chart"""
-
-    class Meta:
-        db_table = 'facebook_chart'
-
-    days = SmallIntegerField(default=14)
-    limit = SmallIntegerField(default=10)
-    facebook_id = CharField(255)
-    facebook_name = CharField(255)
+        dictionary = super().to_dict()
+        dictionary['file'] = self.file
+        return dictionary
 
 
 class HTMLChart(_Chart):
@@ -131,13 +113,62 @@ class HTMLChart(_Chart):
     fullscreen = BooleanField(default=False)
     ken_burns = BooleanField(default=False)
 
+    def to_dict(self):
+        """Returns a JSON compatible dictionary"""
+        dictionary = super().to_dict()
+        dictionary['random'] = self.random
+        dictionary['loop_limit'] = self.loop_limit
+        dictionary['scale'] = self.scale
+        dictionary['fullscreen'] = self.fullscreen
+        dictionary['ken_burns'] = self.ken_burns
+        return dictionary
 
-class VideoChart(_Chart):
-    """A chart that may contain images and texts"""
+
+class FacebookChart(_Chart):
+    """Facebook data chart"""
 
     class Meta:
-        db_table = 'video_chart'
+        db_table = 'facebook_chart'
 
-    file = IntegerField()
-    video = FileProperty(file)
-    schedule = ForeignKeyField(Schedule, db_column='schedule')
+    days = SmallIntegerField(default=14)
+    limit = SmallIntegerField(default=10)
+    facebook_id = CharField(255)
+    facebook_name = CharField(255)
+
+    def to_dict(self):
+        """Returns a JSON compatible dictionary"""
+        dictionary = super().to_dict()
+        dictionary['days'] = self.days
+        dictionary['limit'] = self.limit
+        dictionary['facebook_id'] = self.facebook_id
+        dictionary['facebook_name'] = self.facebook_name
+        return dictionary
+
+
+class GuessPicture(_Chart):
+    """Chart for guessing pictures"""
+
+    class Meta:
+        db_table = 'quotes_chart'
+
+
+class Text(_Chart):
+    """Simple text chart"""
+
+    class Meta:
+        db_table = 'text_chart'
+
+    text = TextField()
+
+    def to_dict(self):
+        """Returns a JSON compatible dictionary"""
+        dictionary = super().to_dict()
+        dictionary['text'] = self.text
+        return dictionary
+
+
+class WeatherChart(_Chart):
+    """Weather data"""
+
+    class Meta:
+        db_table = 'weather_chart'
