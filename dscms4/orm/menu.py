439edb129f432bfc31a34020663f5ed419524dd5
Richard Neumann <r.neumann@homeinfo.de>
2017-04-24 17:40:46 +0200

    Fixing menus

--- dscms4/orm/menu.py
+++ dscms4/orm/menu.py
@@ -7,39 +7,77 @@ from .charts import BaseChart
 
 __all__ = [
     'Menu',
-    'MenuItem',
     'ChartItem']
 
 
 class Menu(DSCMS4Model):
-    """A Menu item"""
-
-    name = CharField(255)
-
-    @property
-    def members(self):
-        """Yields the menu's members"""
-        yield from self.submenus
-        yield from self.charts
-
-
-class MenuItem(DSCMS4Model):
-    """Menus within menus"""
+    """Menus tree nodes"""
 
     class Meta:
-        db_table = 'menu_member'
+        db_table = 'node'
 
-    menu = ForeignKeyField(Menu, db_column='menu', related_name='submenus')
-    member = ForeignKeyField(
-        Menu, db_column='member', related_name='parents')
+    parent = ForeignKeyField(
+        'self', db_column='parent', null=True, default=None)
+    name = CharField(255)
+    text = CharField(255, null=True, default=None)
+
+    @property
+    def root(self):
+        """Determines whether this is a root node entry"""
+        return self.parent is None
+
+    @property
+    def path(self):
+        """Yields the path to this menu"""
+        if not self.root:
+            yield from self.parent.path
+
+        yield self
+
+    @property
+    def submenus(self):
+        """Yields submenus"""
+        return self.__class__.select().where(self.__class__.parent == self)
+
+    @property
+    def charts(self):
+        """Yields charts"""
+        return ChartItem.select().where(ChartItem.parent == self)
+
+    def append(self, name, text=None):
+        """Appends the node"""
+        node = self.__class__()
+        node.parent = self
+        node.name = name
+        node.text = text
+        return node
+
+    def to_dict(self, cascade=False):
+        """Converts the menu into a JSON compliant dictionary"""
+        dictionary = {'name': self.name}
+
+        if self.text is not None:
+            dictionary['text'] = self.text
+
+        charts = [chart.to_dict() for chart in self.charts]
+
+        if charts:
+            dictionary['charts'] = charts
+
+        if cascade:
+            submenus = [m.to_dict(cascade=cascade) for m in self.submenus]
+
+            if submenus:
+                dictionary['submenus'] = submenus
+
+        return dictionary
 
 
 class ChartItem(DSCMS4Model):
-    """Charts within menus"""
+    """Menu item mapping"""
 
     class Meta:
-        db_table = 'chart_member'
+        db_table = 'menu_chart'
 
-    menu = ForeignKeyField(Menu, db_column='menu', related_name='charts')
-    member = ForeignKeyField(
-        BaseChart, db_column='member', related_name='menus')
+    menu = ForeignKeyField(Menu, db_column='menu')
+    chart = ForeignKeyField(BaseChart, db_column='chart')
