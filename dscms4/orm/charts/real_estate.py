11f6e3e0713de3b952060a03ef19a1a19979428f
Richard Neumann <r.neumann@homeinfo.de>
2017-10-26 16:44:09 +0200

    Implemented real estate chart.

---
+++ dscms4/orm/charts/real_estate.py
@@ -0,0 +1,102 @@
+"""Real estate chart ORM."""
+
+from enum import Enum
+
+from peewee import Model, BooleanField, SmallIntegerField, IntegerField
+from peeweeplus import EnumField
+
+from dscms4.orm.charts.common import Chart
+
+
+class DisplayFormat(Enum):
+    """Display formats."""
+
+    BIG_PICTURE = 'big picture'
+    THREE = 'three'
+    FIFTY_FIFTY = 'fifty-fifty'
+
+
+class RealEstate(Model, Chart):
+    """Chart for real estate displaying."""
+
+    display_format = EnumField(DisplayFormat)
+    ken_burns = BooleanField(default=False)
+    scaling = BooleanField(default=False)
+    slideshow = BooleanField(default=True)
+    qr_codes = BooleanField(default=False)
+    show_contact = BooleanField(default=True)
+    contact_picture = BooleanField(default=True)
+    font_size = SmallIntegerField(default=8)
+    font_color = IntegerField(default=0x000000)
+    # Data field selections:
+    amenities = BooleanField(default=True)
+    construction = BooleanField(default=True)
+    courtage = BooleanField(default=True)
+    floor = BooleanField(default=True)
+    area = BooleanField(default=True)
+    free_from = BooleanField(default=True)
+    coop_share = BooleanField(default=True)
+    total_area = BooleanField(default=True)
+    plot_area = BooleanField(default=True)
+    cold_rent = BooleanField(default=True)
+    purchase_price = BooleanField(default=True)
+    security_deposit = BooleanField(default=True)
+    service_charge = BooleanField(default=True)
+    object_id = BooleanField(default=True)
+    description = BooleanField(default=True)
+    warm_rent = BooleanField(default=True)
+    rooms = BooleanField(default=True)
+    # Amenities tags:
+    lift = BooleanField(default=True)
+    bathtub = BooleanField(default=True)
+    balcony = BooleanField(default=True)
+    accessibility = BooleanField(default=True)
+    assited_living = BooleanField(default=True)
+    carport = BooleanField(default=True)
+    floorboards = BooleanField(default=True)
+    duplex = BooleanField(default=True)
+    shower = BooleanField(default=True)
+    builtin_kitchen = BooleanField(default=True)
+    screed = BooleanField(default=True)     # Estrich.
+    tiles = BooleanField(default=True)
+    outdoor_parking = BooleanField(default=True)
+    garage = BooleanField(default=True)
+    cable_sat_tv = BooleanField(default=True)
+    fireplace = BooleanField(default=True)
+    basement = BooleanField(default=True)
+    plastic = BooleanField(default=True)
+    furnished = BooleanField(default=True)
+    parquet = BooleanField(default=True)
+    car_park = BooleanField(default=True)
+    wheelchair_accessible = BooleanField(default=True)
+    sauna = BooleanField(default=True)
+    stone = BooleanField(default=True)
+    swimming_pool = BooleanField(default=True)
+    carpet = BooleanField(default=True)
+    uderground_carpark = BooleanField(default=True)
+    lavatory = BooleanField(default=True)
+    # Rooms selector:
+    rooms_1 = BooleanField(default=True)
+    rooms_2 = BooleanField(default=True)
+    rooms_3 = BooleanField(default=True)
+    rooms_4 = BooleanField(default=True)
+    rooms_5 = BooleanField(default=True)
+    rooms_5_or_more = BooleanField(default=True)
+    # Real estate type:
+    finance_project = BooleanField(default=True)
+    business_realty = BooleanField(default=True)
+    short_term_accommocation = BooleanField(default=True)
+    living_realty = BooleanField(default=True)
+    # Subtypes:
+    office = BooleanField(default=True)
+    retail = BooleanField(default=True)
+    recreational = BooleanField(default=True)
+    hospitality_industry = BooleanField(default=True)
+    plot = BooleanField(default=True)
+    hall_warehouse_production = BooleanField(default=True)
+    house = BooleanField(default=True)
+    agriculture_forestry = BooleanField(default=True)
+    miscellaneous = BooleanField(default=True)
+    flat = BooleanField(default=True)
+    room = BooleanField(default=True)
+    income_property = BooleanField(default=True)
