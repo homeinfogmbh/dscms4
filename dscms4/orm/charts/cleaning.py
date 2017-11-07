9833af8237373d723e1328c09cfba2fac10acc17
Richard Neumann <r.neumann@homeinfo.de>
2017-11-07 12:19:03 +0100

    Added cleaning chart.

---
+++ dscms4/orm/charts/cleaning.py
@@ -0,0 +1,34 @@
+"""Cleaning chart."""
+
+from enum import Enum
+
+from peewee import Model, TextField, SmallIntegerField, IntegerField
+from peeweeplus import EnumField
+
+from dscms4.orm.common import DSCMS4Model
+from dscms4.orm.charts.common import Chart
+
+__all__ = ['Mode', 'Cleaning']
+
+
+DEFAULT_FONT_SIZE = 8
+DEFAULT_COLOR = 0x000000
+
+
+class Mode(Enum):
+    """Possible displaying modes."""
+
+    SHOW = 'show'
+    INPUT = 'input'
+
+
+class Cleaning(Model, Chart):
+    """Cleaning chart."""
+
+    class Meta:
+        db_table = 'chart_cleaning'
+
+    mode = EnumField(Mode)
+    text = TextField(null=True, default=None)
+    font_size = SmallIntegerField(default=DEFAULT_FONT_SIZE)
+    text_color = IntegerField(default=DEFAULT_COLOR)
