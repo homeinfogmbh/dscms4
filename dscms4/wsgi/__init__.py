e75a53b06dc04c28a1b2740d386540f2ec019c7f
Richard Neumann <r.neumann@homeinfo.de>
2017-11-16 14:33:00 +0100

    Added mk_router.

--- dscms4/wsgi/__init__.py
+++ dscms4/wsgi/__init__.py
@@ -7,13 +7,16 @@ from .media import MediaHandler
 from .menu import MenuHandler
 from .ticker import TickerHandler
 
-__all__ = ['HANDLERS']
+__all__ = ['mk_router']
 
 
-HANDLERS = {
-    'chart': CHART_HANDLERS,
-    'configuration': ConfigurationHandler,
-    'group': GROUP_HANDLERS,
-    'media': MediaHandler,
-    'menu': MenuHandler,
-    'ticker': TickerHandler}
+def mk_router(root):
+    """Generates a router for the respective root."""
+
+    return Router(
+        (Route('{}/chart/[id:int]'), CHART_HANDLERS),
+        (Route('{}/configuration/[id:int]'), ConfigurationHandler),
+        (Route('{}/group/[id:int]'), GROUP_HANDLERS),
+        (Route('{}/media/[id:int]'), MediaHandler),
+        (Route('{}/menu/[id:int]'), MenuHandler),
+        (Route('{}/ticker/[id:int]'), TickerHandler))
