18fa0b3bb7aeb827d4bfa7ad10b1cd7ff95b3687
Richard Neumann <r.neumann@homeinfo.de>
2018-01-05 13:58:02 +0100

    Implemented routes setting.

--- dscms4/wsgi/__init__.py
+++ dscms4/wsgi/__init__.py
@@ -1,31 +1,16 @@
 """WSGI application routes."""
 
+from itertools import chain
+
 from wsgilib import Application
 
-from dscms4.wsgi import configuration, content, charts, group, media, menu, \
+from dscms4.wsgi import charts, configuration, content, group, media, menu, \
     terminal
 
 __all__ = ['APPLICATION']
 
 
 APPLICATION = Application('DSCMS4')
-ROUTES =
-
-for route, method, function in chain(
-        configuration.ROUTES, content.ROUTES, charts.ROUTES):
-        # Configuration endpoint.
-        # Content endpoint.
-        # Charts endpoint.
-        # Group endpoint.,
-        # Media endpoint.
-        # TODO: implement.
-        # Menu endpoint.
-        ('/menu', 'GET', menu.list),
-        ('/menu/<int:ident>', 'GET', menu.get),
-        ('/menu', 'POST', menu.add),
-        ('/menu/<int:gid>', 'PATCH', menu.patch),
-        ('/menu/<int:gid>', 'DELETE', menu.delete),
-        # Terminal endpoint.
-        ('/terminals', 'GET', terminal.list),
-        ('/terminals/<int:tid>', 'GET', terminal.get)):
-    APPLICATION.route(route, methods=[method])(function)
+APPLICATION.add_routes(chain(
+    charts.ROUTES, configuration.ROUTES, content.ROUTES, group.ROUTES,
+    media.ROUTES, menu.ROUTES, terminal.ROUTES))
