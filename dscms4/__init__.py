d886dfd34fba00d205660533cfa11296a652bff7
Richard Neumann <r.neumann@homeinfo.de>
2018-01-11 17:32:21 +0100

    Edited docstring.

--- dscms4/__init__.py
+++ dscms4/__init__.py
@@ -1,5 +1,7 @@
 """DSCMS 4.
 
-Web application backend for the
 Digital Signage Content Management System version 4.
+
+A web application backend to edit and
+publish digital signage applications.
 """
