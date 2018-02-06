0bc7feecbc54f1ba220413dc6de55f5155d32899
Richard Neumann <r.neumann@homeinfo.de>
2018-02-06 10:47:45 +0100

    Implemented util.create_tables().

--- dscms4/orm/util.py
+++ dscms4/orm/util.py
@@ -3,21 +3,33 @@
 from collections import namedtuple
 from sys import stderr
 
-from dscms4.orm.charts import CHARTS
+from dscms4.orm import charts, content, chart_types, configuration, group, \
+    menu, schedule, tags, ticker
 from dscms4.orm.charts.common import BaseChart
 from dscms4.orm.exceptions import OrphanedBaseChart, AmbiguousBaseChart
 
 
-__all__ = ['charts_of', 'chart_of', 'check_base_charts']
+__all__ = ['create_tables', 'charts_of', 'chart_of', 'check_base_charts']
 
 
+MODELS = (
+    charts.MODELS + content.MODELS + chart_types.MODELS + configuration.MODELS
+    + group.MODELS + menu.MODELS + schedule.MODELS + tags.MODELS
+    + ticker.MODELS)
 CheckResult = namedtuple('CheckResult', ('orphans', 'ambiguous'))
+
+
+def create_tables(*args, **kwargs):
+    """Creates the respective database tables."""
+
+    for model in MODELS:
+        model.create_table(*args, **kwargs)
 
 
 def charts_of(base_chart):
     """Yields all charts that associate this base chart."""
 
-    for _, cls in CHARTS.items():
+    for cls in charts.CHARTS.values():
         for chart in cls.select().where(cls.base_chart == base_chart):
             yield chart
 
@@ -47,10 +59,14 @@ def check_base_charts(verbose=False):
             chart = chart_of(base_chart)
         except OrphanedBaseChart as orphaned_base_chart:
             orphans.append(base_chart)
-            print(orphaned_base_chart, file=stderr)
+
+            if verbose:
+                print(orphaned_base_chart, file=stderr)
         except AmbiguousBaseChart as ambiguous_base_chart:
             ambiguous.append(base_chart)
-            print(ambiguous_base_chart, file=stderr)
+
+            if verbose:
+                print(ambiguous_base_chart, file=stderr)
         else:
             if verbose:
                 print(base_chart, '↔', chart)
