f34e13ce2424363a158d88cd9b4e105751a4c8c3
Richard Neumann <r.neumann@homeinfo.de>
2018-02-06 11:44:23 +0100

    Removed orphaned media imports.

--- dscms4/wsgi/__init__.py
+++ dscms4/wsgi/__init__.py
@@ -4,8 +4,7 @@ from itertools import chain
 
 from wsgilib import Application
 
-from dscms4.wsgi import charts, configuration, content, group, media, menu, \
-    terminal
+from dscms4.wsgi import charts, configuration, content, group, menu, terminal
 
 __all__ = ['APPLICATION']
 
@@ -13,4 +12,4 @@ __all__ = ['APPLICATION']
 APPLICATION = Application('DSCMS4')
 APPLICATION.add_routes(chain(
     charts.ROUTES, configuration.ROUTES, content.ROUTES, group.ROUTES,
-    media.ROUTES, menu.ROUTES, terminal.ROUTES))
+    menu.ROUTES, terminal.ROUTES))
