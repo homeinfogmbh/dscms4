2a4dc0065d42a88874ecf3d3c3e9b27659b210fe
Richard Neumann <r.neumann@homeinfo.de>
2018-02-06 11:39:05 +0100

    Updated locale path.

--- dscms4/messages/terminal.py
+++ dscms4/messages/terminal.py
@@ -8,7 +8,7 @@ __all__ = ['NoSuchTerminal']
 class TerminalMessage(Message):
     """Base class for terminal related messages."""
 
-    LOCALES = locales('/etc/dscms4.d/locales/menu.ini')
+    LOCALES = locales('/etc/dscms4.d/locales/terminal.ini')
     ABSTRACT = True
 
 
