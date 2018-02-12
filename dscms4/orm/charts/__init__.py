6eabd122d9addcded24ce9860d69ef8098431240
Richard Neumann <r.neumann@homeinfo.de>
2018-02-12 15:52:20 +0100

    Added missing form chart.

--- dscms4/orm/charts/__init__.py
+++ dscms4/orm/charts/__init__.py
@@ -5,6 +5,7 @@ of the "chart" types of the CMS.
 """
 from dscms4.orm.charts.common import BaseChart, Chart
 from dscms4.orm.charts.facebook import Facebook, Account
+from dscms4.orm.charts.form import Form
 from dscms4.orm.charts.guess_picture import GuessPicture
 from dscms4.orm.charts.image_text import ImageText, Image, Text
 from dscms4.orm.charts.news import News
@@ -18,7 +19,7 @@ __all__ = ['MODELS', 'CHARTS']
 
 
 MODELS = (
-    BaseChart, Facebook, Account, GuessPicture, ImageText, Image, Text, News,
-    PublicTransport, Quotes, Video, Weather)
+    BaseChart, Facebook, Form, Account, GuessPicture, ImageText, Image, Text,
+    News, PublicTransport, Quotes, Video, Weather)
 CHARTS = {
     model.__name__: model for model in MODELS if issubclass(model, Chart)}
