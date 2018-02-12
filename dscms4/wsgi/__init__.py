51480367438c5c3ddc6edbc2f6d31d9806abb438
Richard Neumann <r.neumann@homeinfo.de>
2018-02-12 13:38:25 +0100

    Enabled CORS and debug mode.

--- dscms4/wsgi/__init__.py
+++ dscms4/wsgi/__init__.py
@@ -9,7 +9,7 @@ from dscms4.wsgi import charts, configuration, content, group, menu, terminal
 __all__ = ['APPLICATION', 'ROUTES']
 
 
-APPLICATION = Application('DSCMS4')
+APPLICATION = Application('DSCMS4', cors=True, debug=True)
 ROUTES = (
     charts.ROUTES + configuration.ROUTES + content.ROUTES + group.ROUTES
     + menu.ROUTES + terminal.ROUTES)
