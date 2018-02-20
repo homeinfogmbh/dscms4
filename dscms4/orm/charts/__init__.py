d009b957c9b615df30497cd10eaf2cf4b5bd6502
Richard Neumann <r.neumann@homeinfo.de>
2018-02-20 10:17:53 +0100

    Added missing chart types.

--- dscms4/orm/charts/__init__.py
+++ dscms4/orm/charts/__init__.py
@@ -3,9 +3,11 @@
 This package provides ORM models
 of the "chart" types of the CMS.
 """
+from dscms4.orm.charts.cleaning import Cleaning
 from dscms4.orm.charts.common import ChartGroup, BaseChart, Chart
 from dscms4.orm.charts.facebook import Facebook, Account
 from dscms4.orm.charts.form import Form
+from dscms4.orm.charts.garbage_collection import GarbageCollection
 from dscms4.orm.charts.guess_picture import GuessPicture
 from dscms4.orm.charts.image_text import ImageText, Image, Text
 from dscms4.orm.charts.news import News
@@ -19,8 +21,9 @@ __all__ = ['MODELS', 'CHARTS']
 
 
 MODELS = (
-    BaseChart, Facebook, Form, Account, GuessPicture, ImageText, Image, Text,
-    News, PublicTransport, Quotes, Video, Weather)
+    BaseChart, Cleaning, Facebook, Account, Form, GarbageCollection,
+    GuessPicture, ImageText, Image, Text, News, PublicTransport, Quotes, Video,
+    Weather)
 CHARTS = {
     model.__name__: model for model in MODELS if issubclass(model, Chart)}
 
