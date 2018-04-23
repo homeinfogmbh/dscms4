cba2208d394cc1afc0ae87450f1097a74322b7b2
Richard Neumann <r.neumann@homeinfo.de>
2018-04-23 12:17:57 +0200

    Added menu item charts routes.

--- dscms4/wsgi/menu/__init__.py
+++ dscms4/wsgi/menu/__init__.py
@@ -1,9 +1,9 @@
 """DSCMS4 WSGI handlers for menus."""
 
-from dscms4.wsgi.menu import menu, item
+from dscms4.wsgi.menu import menu, item, charts
 from dscms4.wsgi.menu.menu import get_menu
 
 __all__ = ['get_menu', 'ROUTES']
 
 
-ROUTES = menu.ROUTES + item.ROUTES
+ROUTES = menu.ROUTES + item.ROUTES + charts.ROUTES
