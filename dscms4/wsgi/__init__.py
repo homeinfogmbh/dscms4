c5344c4446b145b219dc445d77eeeee97a622b8c
Richard Neumann <r.neumann@homeinfo.de>
2018-05-09 14:21:58 +0200

    Added file endpoints.

--- dscms4/wsgi/__init__.py
+++ dscms4/wsgi/__init__.py
@@ -4,13 +4,14 @@ from itertools import chain
 
 from wsgilib import Application
 
-from dscms4.wsgi import charts, configuration, content, group, menu, terminal
+from dscms4.wsgi import charts, configuration, content, file, group, menu, \
+    terminal
 
 __all__ = ['APPLICATION', 'ROUTES']
 
 
 APPLICATION = Application('DSCMS4', cors=True, debug=True)
 ROUTES = (
-    charts.ROUTES + configuration.ROUTES + content.ROUTES + group.ROUTES
-    + menu.ROUTES + terminal.ROUTES)
+    charts.ROUTES + configuration.ROUTES + content.ROUTES + file.ROUTES
+    + group.ROUTES + menu.ROUTES + terminal.ROUTES)
 APPLICATION.add_routes(ROUTES)
