243fe8067bb20beb8660438322241acf9cc27326
Richard Neumann <r.neumann@homeinfo.de>
2018-06-06 12:11:37 +0200

    Using type_ property now.

--- dscms4/orm/charts/__init__.py
+++ dscms4/orm/charts/__init__.py
@@ -26,5 +26,4 @@ MODELS = (
     BaseChart, Cleaning, Facebook, Account, Form, GarbageCollection,
     GuessPicture, ImageText, Image, Text, News, PublicTransport, Quotes,
     RealEstates, IdFilter, ZipCodeFilter, Video, Weather, Image_)
-CHARTS = {
-    model.__name__: model for model in MODELS if issubclass(model, Chart)}
+CHARTS = {model.type_: model for model in MODELS if issubclass(model, Chart)}
