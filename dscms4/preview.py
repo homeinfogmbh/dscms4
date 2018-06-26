649fdd90e8192fa6f169aa633b99d0c86045bc00
Richard Neumann <r.neumann@homeinfo.de>
2018-06-26 14:46:05 +0200

    fix

--- dscms4/preview.py
+++ dscms4/preview.py
@@ -43,14 +43,13 @@ def file_preview(presentation_class):
         def wrapper(obj, ident, *args, **kwargs):
             """Receives a token and arguments for the original function."""
             presentation = presentation_class(obj)
-            files = presentation.file_set()
 
-            if ident in files:
+            if ident in presentation.files:
                 file = File.get(
                     (File.id == ident) & (File.customer == presentation.cid))
                 return function(file, *args, **kwargs)
 
-            raise Unauthorized(files=list(files))
+            raise Unauthorized(files=list(presentation.files))
 
         return wrapper
 
