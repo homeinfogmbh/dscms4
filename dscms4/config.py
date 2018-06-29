3b3c4499a3008996af6de2e1a909a955df429310
Richard Neumann <r.neumann@homeinfo.de>
2018-06-29 14:43:45 +0200

    Added logging.

--- dscms4/config.py
+++ dscms4/config.py
@@ -2,7 +2,8 @@
 
 from configlib import INIParser
 
-__all__ = ['CONFIG']
+__all__ = ['LOG_FORMAT', 'CONFIG']
 
 
+LOG_FORMAT = '[%(levelname)s] %(name)s: %(message)s'
 CONFIG = INIParser('/etc/dscms4.conf')
--- dscms4/file_hooks.py
+++ dscms4/file_hooks.py
@@ -1,5 +1,8 @@
 """File hooks."""
 
+from logging import INFO, basicConfig, getLogger
+
+from dscms4.config import LOG_FORMAT
 from dscms4.orm.charts.image_text import Image as ITCImage
 from dscms4.orm.charts.video import Video
 from dscms4.orm.charts.weather import Image as WCImage
@@ -8,22 +11,35 @@ from dscms4.orm.configuration import Configuration
 __all__ = ['on_delete']
 
 
+LOGGER = getLogger('dscms4.hooks')
+basicConfig(level=INFO, format=LOG_FORMAT)
+
+
 def _remove_itc_images(ident):
     """Removes the respective Images of ImageTest charts."""
 
-    ITCImage.delete().where(ITCImage.image == ident)
+    for image in ITCImage.select().where(ITCImage.image == ident):
+        LOGGER.info(
+            'Deleting image_text.Image %i with image %i.',
+            image.id, ident)
+        image.delete_instance()
 
 
 def _remove_wc_image(ident):
     """Removes the respective Images of Weather charts."""
 
-    WCImage.delete().where(WCImage.image == ident)
+    for image in WCImage.delete().where(WCImage.image == ident):
+        LOGGER.info(
+            'Deleting weather.Image %i with image %i.',
+            image.id, ident)
+        image.delete_instance()
 
 
 def _remove_video_charts(ident):
     """Removes the respective video charts."""
 
     for video in Video.select().where(Video.video == ident):
+        LOGGER.info('Setting video = %i to NULL on Video %i.', ident, video.id)
         video.video = None
         video.save()
 
@@ -38,12 +54,21 @@ def _null_configurations(ident):
             | (Configuration.background == ident)
             | (Configuration.dummy_picture == ident)):
         if configuration.logo == ident:
+            LOGGER.info(
+                'Setting logo = %i to NULL on Configuration %i.',
+                ident, configuration.id)
             configuration.logo = None
 
         if configuration.background == ident:
+            LOGGER.info(
+                'Setting background = %i to NULL on Configuration %i.',
+                ident, configuration.id)
             configuration.background = None
 
         if configuration.dummy_picture == ident:
+            LOGGER.info(
+                'Setting dummy_picture = %i to NULL on Configuration %i.',
+                ident, configuration.id)
             configuration.dummy_picture = None
 
         configuration.save()
