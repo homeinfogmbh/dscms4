35655000faed1bfeecf038dba16203f0d3c70f9c
Richard Neumann <r.neumann@homeinfo.de>
2018-08-17 09:49:37 +0200

    Refactored CustomerModel.select() override.

--- dscms4/orm/common.py
+++ dscms4/orm/common.py
@@ -21,15 +21,6 @@ __all__ = [
 DATABASE = MySQLDatabase.from_config(CONFIG['db'])
 
 
-def _log_warning():
-    """Logs warning and info message if no request context is
-    available and filtering for customer context is omitted.
-    """
-
-    LOGGER.warning('Querying outside of request context.')
-    LOGGER.info('Will not filter for customers.')
-
-
 def create_tables(models, fail_silently=True):
     """Creates the tables for the provided models."""
 
@@ -70,18 +61,6 @@ class CustomerModel(DSCMS4Model):
         return record
 
     @classmethod
-    def get(cls, *query, **filters):
-        """Returns the respective records for the customer in the current
-        HIS context iff no customer was explicitely specified in the filters.
-        """
-        if has_request_context():
-            return super().get(*query, customer=CUSTOMER.id, **filters)
-
-        _log_warning()
-        return super().get(*query, **filters)
-
-
-    @classmethod
     def select(cls, *fields):
         """Returns a selection with additional filtering for
         the current HIS customer iff within HIS context.
@@ -89,11 +68,10 @@ class CustomerModel(DSCMS4Model):
         model_select = super().select(*fields)
 
         if has_request_context():
-            where = cls.customer == CUSTOMER.id
-            model_select._where = where     # pylint: disable=W0212
-        else:
-            _log_warning()
+            return model_select.where(cls.customer == CUSTOMER.id)
 
+        LOGGER.warning('Querying outside of request context.')
+        LOGGER.warning('Will not filter for current customer.')
         return model_select
 
     def get_peer(self, record_or_id):
