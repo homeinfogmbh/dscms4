1f1ebfa5618bfcbfdbf38434b50791912723efa5
Richard Neumann <r.neumann@homeinfo.de>
2018-08-20 11:11:14 +0200

    fix

--- dscms4/wsgi/menu/__init__.py
+++ dscms4/wsgi/menu/__init__.py
@@ -1,9 +1,8 @@
 """DSCMS4 WSGI handlers for menus."""
 
 from dscms4.wsgi.menu import menu, item, charts
-from dscms4.wsgi.menu.menu import get_menu
 
-__all__ = ['get_menu', 'ROUTES']
+__all__ = ['ROUTES']
 
 
 ROUTES = menu.ROUTES + item.ROUTES + charts.ROUTES
