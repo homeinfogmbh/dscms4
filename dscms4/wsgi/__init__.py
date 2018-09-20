248f979698346186d1ff77a10e10ae7c39ad5faa
Richard Neumann <r.neumann@homeinfo.de>
2018-09-20 11:13:23 +0200

    Added management endpoint.

--- dscms4/wsgi/__init__.py
+++ dscms4/wsgi/__init__.py
@@ -4,8 +4,16 @@ from itertools import chain
 
 from his import Application
 
-from dscms4.wsgi import charts, configuration, content, group, membership, \
-    menu, preview, previewgen, terminal
+from dscms4.wsgi import charts
+from dscms4.wsgi import configuration
+from dscms4.wsgi import content
+from dscms4.wsgi import group
+from dscms4.wsgi import management
+from dscms4.wsgi import membership
+from dscms4.wsgi import menu
+from dscms4.wsgi import preview
+from dscms4.wsgi import previewgen
+from dscms4.wsgi import terminal
 
 
 __all__ = ['APPLICATION', 'ROUTES']
@@ -14,6 +22,6 @@ __all__ = ['APPLICATION', 'ROUTES']
 APPLICATION = Application('DSCMS4', cors=True, debug=True)
 ROUTES = (
     charts.ROUTES + configuration.ROUTES + content.ROUTES + group.ROUTES
-    + membership.ROUTES + menu.ROUTES + preview.ROUTES + previewgen.ROUTES
-    + terminal.ROUTES)
+    + management.ROUTES + membership.ROUTES + menu.ROUTES + preview.ROUTES
+    + previewgen.ROUTES + terminal.ROUTES)
 APPLICATION.add_routes(ROUTES)
