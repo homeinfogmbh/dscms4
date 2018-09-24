ed915a07e2808c0857e8bdd3d6a74a6bb311ce94
Richard Neumann <r.neumann@homeinfo.de>
2018-09-24 10:18:47 +0200

    Implemented settings endpoint.

---
+++ dscms4/wsgi/settings.py
@@ -0,0 +1,35 @@
+"""Per-customer settings interface."""
+
+from his import CUSTOMER, admin, authenticated, authorized
+
+from dscms4.messages.settings import SettingsSaved
+from dscms4.orm.settings import Settings
+
+
+__all__ = ['ROUTES']
+
+
+@authenlicated
+@authorized('dscms4')
+def get_settings():
+    """Gets the customer's settings."""
+
+    settings = Settings.for_customer(CUSTOMER.id)
+    return JSON(settings.to_json())
+
+
+@authenlicated
+@authorized('dscms4')
+@admin
+def set_settings():
+    """Sets the customer's settings."""
+
+    settings = Settings.for_customer(CUSTOMER.id)
+    settings.patch(request.json4)
+    settings.save()
+    return SettingsSaved()
+
+
+ROUTES = (
+    ('/GET', '/settings', get_settings, 'get_settings'),
+    ('/POST', '/settings', set_settings, 'set_settings'))
