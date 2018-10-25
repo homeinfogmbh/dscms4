d138070ba3eaa668cecf22478ca112e161ee8e03
Richard Neumann <r.neumann@homeinfo.de>
2018-10-25 16:12:49 +0200

    Sorting charts by ID now.

--- dscms4/presentation.py
+++ dscms4/presentation.py
@@ -163,6 +163,7 @@ class Presentation:
             (BaseChart.trashed == 0) & (MenuItem.menu << self.menus))
 
     @property
+    @coerce(partial(sorted, key=lambda chart: chart.id))
     @coerce(frozenset)
     def charts(self):
         """Yields all charts for this terminal."""
