895d45e3ee4cf45d5c15d2155fe91511107227c8
Richard Neumann <r.neumann@homeinfo.de>
2018-12-07 12:58:05 +0100

    Refac.

--- dscms4/config.py
+++ dscms4/config.py
@@ -2,13 +2,13 @@
 
 from logging import INFO, basicConfig, getLogger
 
-from configlib import INIParser
+from configlib import loadcfg
 
 
 __all__ = ['CONFIG', 'LOG_FORMAT', 'LOGGER']
 
 
-CONFIG = INIParser('/usr/local/etc/dscms4.conf')
+CONFIG = loadcfg('dscms4.conf')
 LOG_FORMAT = '[%(levelname)s] %(name)s: %(message)s'
 basicConfig(level=INFO, format=LOG_FORMAT)
 LOGGER = getLogger('dscms4')
