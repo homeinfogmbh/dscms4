e9e4defdf7d18bc7bc715dbb40a2b5f4d7bf5700
Richard Neumann <r.neumann@homeinfo.de>
2019-01-09 13:58:37 +0100

    Updated dependencies.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author_email='<info at homeinfo dot de>',
     maintainer='Richard Neumann',
     maintainer_email='<r dot neumann at homeinfo period de>',
-    requires=['his'],
+    requires=['his', 'cmslib'],
     packages=[
         'dscms4',
         'dscms4.content',
