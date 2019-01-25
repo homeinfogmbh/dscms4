3d2093c0e272c915a8b086e1a44d248ab4c852cb
Richard Neumann <r.neumann@homeinfo.de>
2019-01-25 12:05:08 +0100

    Implemented ComCat account interface.

--- dscms4/__init__.py
+++ dscms4/__init__.py
@@ -9,6 +9,7 @@ from itertools import chain
 from his import Application
 
 from dscms4 import charts
+from dscms4 import comcat_account
 from dscms4 import configuration
 from dscms4 import content
 from dscms4 import group
@@ -25,7 +26,7 @@ __all__ = ['APPLICATION', 'ROUTES']
 
 APPLICATION = Application('DSCMS4', debug=True)
 ROUTES = (
-    charts.ROUTES + configuration.ROUTES + content.ROUTES + group.ROUTES
-    + membership.ROUTES + menu.ROUTES + preview.ROUTES + previewgen.ROUTES
-    + settings.ROUTES + terminal.ROUTES)
+    charts.ROUTES + comcat_account.ROUTES + configuration.ROUTES
+    + content.ROUTES + group.ROUTES + membership.ROUTES + menu.ROUTES
+    + preview.ROUTES + previewgen.ROUTES + settings.ROUTES + terminal.ROUTES)
 APPLICATION.add_routes(ROUTES)
