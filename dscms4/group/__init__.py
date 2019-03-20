7b8cd9d59fa1d702b11024fae9a4feff5954f0b3
Richard Neumann <r.neumann@homeinfo.de>
2019-03-20 09:35:48 +0100

    fix

--- dscms4/group/__init__.py
+++ dscms4/group/__init__.py
@@ -1,9 +1,9 @@
 """Handling of groups and group members."""
 
-from dscms4.group import group, member, tree
+from dscms4.group import group, terminal, tree
 
 
 __all__ = ['ROUTES']
 
 
-ROUTES = group.ROUTES + member.ROUTES + tree.ROUTES
+ROUTES = group.ROUTES + terminal.ROUTES + tree.ROUTES
