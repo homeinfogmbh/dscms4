8df9c7933c1518a8cbf3866fb4949f811a370e2b
Richard Neumann <r.neumann@homeinfo.de>
2019-04-17 11:30:04 +0200

    Fix.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
         'dscms4',
         'dscms4.content',
         'dscms4.content.group',
-        'dscms4.content.terminal',
+        'dscms4.content.system',
         'dscms4.group',
         'dscms4.membership',
         'dscms4.menu',
