b561354d19a50fedf0501e23ab25ad3ad2bde4c9
Richard Neumann <r.neumann@homeinfo.de>
2019-09-25 15:14:31 +0200

    Refac.

--- dscms4/preview/deployment.py
+++ dscms4/preview/deployment.py
@@ -1,13 +1,8 @@
 """Preview for deployments."""
 
-from cmslib.exceptions import AmbiguousConfigurationsError
-from cmslib.exceptions import NoConfigurationFound
-from cmslib.messages.presentation import NO_CONFIGURATION_ASSIGNED
-from cmslib.messages.presentation import AMBIGUOUS_CONFIGURATIONS
 from cmslib.presentation.deployment import Presentation
-from his.messages.request import INVALID_CONTENT_TYPE
-from previewlib import preview, file_preview, DeploymentPreviewToken
-from wsgilib import ACCEPT, JSON, XML, Binary
+from cmslib.preview import make_response
+from previewlib import preview, DeploymentPreviewToken
 
 
 __all__ = ['ROUTES']
@@ -17,31 +12,7 @@ __all__ = ['ROUTES']
 def get_presentation(deployment):
     """Returns the presentation for the respective deployment."""
 
-    presentation = Presentation(deployment)
-
-    if  'application/xml' in ACCEPT or '*/*' in ACCEPT:
-        try:
-            return XML(presentation.to_dom())
-        except AmbiguousConfigurationsError:
-            return AMBIGUOUS_CONFIGURATIONS
-        except NoConfigurationFound:
-            return NO_CONFIGURATION_ASSIGNED
-
-    if 'application/json' in ACCEPT:
-        return JSON(presentation.to_json())
-
-    return INVALID_CONTENT_TYPE
+    return make_response(Presentation(deployment))
 
 
-@preview(DeploymentPreviewToken)
-@file_preview(Presentation)
-def get_file(file):
-    """Returns the presentation for the respective deployment."""
-
-    return Binary(file.bytes)
-
-
-ROUTES = (
-    ('GET', '/preview/deployment', get_presentation),
-    ('GET', '/preview/deployment/<int:ident>', get_file)
-)
+ROUTES = (('GET', '/preview/deployment', get_presentation),)
--- dscms4/preview/group.py
+++ dscms4/preview/group.py
@@ -1,13 +1,8 @@
 """Preview for groups."""
 
-from cmslib.exceptions import AmbiguousConfigurationsError
-from cmslib.exceptions import NoConfigurationFound
-from cmslib.messages.presentation import NO_CONFIGURATION_ASSIGNED
-from cmslib.messages.presentation import AMBIGUOUS_CONFIGURATIONS
 from cmslib.presentation.group import Presentation
-from his.messages.request import INVALID_CONTENT_TYPE
-from previewlib import preview, file_preview, GroupPreviewToken
-from wsgilib import ACCEPT, JSON, XML, Binary
+from cmslib.preview import make_response
+from previewlib import preview, GroupPreviewToken
 
 
 __all__ = ['ROUTES']
@@ -17,31 +12,7 @@ __all__ = ['ROUTES']
 def get_presentation(group):
     """Returns the presentation for the respective group."""
 
-    presentation = Presentation(group)
-
-    if  'application/xml' in ACCEPT or '*/*' in ACCEPT:
-        try:
-            return XML(presentation.to_dom())
-        except AmbiguousConfigurationsError:
-            return AMBIGUOUS_CONFIGURATIONS
-        except NoConfigurationFound:
-            return NO_CONFIGURATION_ASSIGNED
-
-    if 'application/json' in ACCEPT:
-        return JSON(presentation.to_json())
-
-    return INVALID_CONTENT_TYPE
+    return make_response(Presentation(group))
 
 
-@preview(GroupPreviewToken)
-@file_preview(Presentation)
-def get_file(file):
-    """Returns a file of the respective presentaion."""
-
-    return Binary(file.bytes)
-
-
-ROUTES = (
-    ('GET', '/preview/group', get_presentation),
-    ('GET', '/preview/group/<int:ident>', get_file)
-)
+ROUTES = (('GET', '/preview/group', get_presentation),)
