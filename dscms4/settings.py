c68aae57f3a356193630f97e262665e612b08aba
Richard Neumann <mail@richard-neumann.de>
2021-01-21 14:40:46 +0100

    Refac.

--- dscms4/settings.py
+++ dscms4/settings.py
@@ -1,8 +1,9 @@
 """Per-customer settings interface."""
 
-from cmslib.messages.settings import SETTINGS_SAVED
+from flask import request
+
 from cmslib.orm.settings import Settings
-from his import CUSTOMER, JSON_DATA, admin, authenticated, authorized
+from his import CUSTOMER, admin, authenticated, authorized, require_json
 from wsgilib import JSON, JSONMessage
 
 
@@ -11,27 +12,27 @@ __all__ = ['ROUTES']
 
 @authenticated
 @authorized('dscms4')
-def get_settings() -> JSON:
+def get() -> JSON:
     """Gets the customer's settings."""
 
-    settings = Settings.for_customer(CUSTOMER.id)
-    return JSON(settings.to_json())
+    return JSON(Settings.for_customer(CUSTOMER.id).to_json())
 
 
 @authenticated
 @authorized('dscms4')
 @admin
-def set_settings() -> JSONMessage:
+@require_json(dict)
+def patch() -> JSONMessage:
     """Sets the customer's settings."""
 
-    json = dict(JSON_DATA)
     settings = Settings.for_customer(CUSTOMER.id)
-    settings.patch_json(json)
+    settings.patch_json(request.json)
     settings.save()
-    return SETTINGS_SAVED
+    return JSONMessage('Settings saved.', status=200)
 
 
-ROUTES = (
-    ('GET', '/settings', get_settings),
-    ('POST', '/settings', set_settings)
-)
+ROUTES = [
+    ('GET', '/settings', get),
+    ('POST', '/settings', patch),
+    ('PATCH', '/settings', patch)
+]
