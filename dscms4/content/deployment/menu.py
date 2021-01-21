ef3c220ccda1f64d070c51cfb7837827096da674
Richard Neumann <mail@richard-neumann.de>
2021-01-21 12:47:13 +0100

    Re-wrote deployment menu.

--- dscms4/content/deployment/menu.py
+++ dscms4/content/deployment/menu.py
@@ -1,13 +1,13 @@
 """Management of menus in deployments."""
 
+from flask import request
+
 from cmslib.functions.menu import get_menu
+from cmslib.functions.content import get_deployment_menu
+from cmslib.functions.content import get_deployment_menus
 from cmslib.functions.deployment import get_deployment
-from cmslib.messages.content import CONTENT_ADDED
-from cmslib.messages.content import CONTENT_DELETED
-from cmslib.messages.content import CONTENT_EXISTS
-from cmslib.messages.content import NO_SUCH_CONTENT
 from cmslib.orm.content.deployment import DeploymentMenu
-from his import authenticated, authorized
+from his import authenticated, authorized, require_json
 from wsgilib import JSON, JSONMessage
 
 
@@ -16,58 +16,51 @@ __all__ = ['ROUTES']
 
 @authenticated
 @authorized('dscms4')
-def get(deployment: int) -> JSON:
-    """Returns a list of IDs of the menus in the respective deployment."""
+def list_() -> JSON:
+    """Lists deployment menus."""
 
-    return JSON([
-        deployment_menu.menu.id for deployment_menu
-        in DeploymentMenu.select().where(
-            DeploymentMenu.deployment == get_deployment(deployment))])
+    deployment = request.args.get('deployment')
+
+    if deployment is not None:
+        deployment = int(deployment)
+
+    return JSON([record.to_json() for record in get_deployment_menus(
+        deployment=deployment)])
 
 
 @authenticated
 @authorized('dscms4')
-def add(deployment: int, menu: int) -> JSONMessage:
+def get(ident: int) -> JSON:
+    """Returns the requested deployment menu."""
+
+    return JSON(get_deployment_menu(ident).to_json())
+
+
+@authenticated
+@authorized('dscms4')
+@require_json(dict)
+def add() -> JSONMessage:
     """Adds the menu to the respective deployment."""
 
-    deployment = get_deployment(deployment)
-    menu = get_menu(menu)
-
-    try:
-        DeploymentMenu.get(
-            (DeploymentMenu.deployment == deployment)
-            & (DeploymentMenu.menu == menu))
-    except DeploymentMenu.DoesNotExist:
-        deployment_menu = DeploymentMenu()
-        deployment_menu.deployment = deployment
-        deployment_menu.menu = menu
-        deployment_menu.save()
-        return CONTENT_ADDED
-
-    return CONTENT_EXISTS
+    deployment = get_deployment(request.json.pop('deployment'))
+    menu = get_menu(request.json.pop('menu'))
+    record = DeploymentMenu(deployment=deployment, menu=menu)
+    record.save()
+    return JSONMessage('Deplyoment menu added.', id=record.id, status=201)
 
 
 @authenticated
 @authorized('dscms4')
-def delete(deployment: int , menu: int) -> JSONMessage:
+def delete(ident: int) -> JSONMessage:
     """Deletes the menu from the respective deployment."""
 
-    deployment = get_deployment(deployment)
-    menu = get_menu(menu)
-
-    try:
-        deployment_menu = DeploymentMenu.get(
-            (DeploymentMenu.deployment == deployment)
-            & (DeploymentMenu.menu == menu))
-    except DeploymentMenu.DoesNotExist:
-        return NO_SUCH_CONTENT
-
-    deployment_menu.delete_instance()
-    return CONTENT_DELETED
+    get_deployment_menu(ident).delete_instance()
+    return JSONMessage('Deployment menu deleted.', status=200)
 
 
-ROUTES = (
-    ('GET', '/content/deployment/<int:deployment>/menu', get),
-    ('POST', '/content/deployment/<int:deployment>/menu/<int:menu>', add),
-    ('DELETE', '/content/deployment/<int:deployment>/menu/<int:menu>', delete)
-)
+ROUTES = [
+    ('GET', '/content/deployment/menu/', list_),
+    ('GET', '/content/deployment/menu/<int:ident>', get),
+    ('POST', '/content/deployment/menu', add),
+    ('DELETE', '/content/deployment/menu/<int:ident>', delete)
+]
