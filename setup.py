e38544e8de46417cff8ea6b11dc4b724847b5ec4
Richard Neumann <mail@richard-neumann.de>
2021-01-21 14:45:03 +0100

    Migrated to setuptools.

--- setup.py
+++ setup.py
@@ -1,16 +1,29 @@
 #! /usr/bin/env python3
+"""Install script."""
 
-from distutils.core import setup
+from setuptools import setup
 
 
 setup(
     name='dscms4',
-    version='latest',
+    use_scm_version={
+        "local_scheme": "node-and-timestamp"
+    },
+    setup_requires=['setuptools_scm'],
     author='HOMEINFO - Digitale Informationssysteme GmbH',
     author_email='<info at homeinfo dot de>',
     maintainer='Richard Neumann',
     maintainer_email='<r dot neumann at homeinfo period de>',
-    requires=['his', 'cmslib'],
+    install_requires=[
+        'cmslib',
+        'flask',
+        'his',
+        'hwdb',
+        'mdb',
+        'peewee',
+        'previewlib',
+        'wsgilib'
+    ],
     packages=[
         'dscms4',
         'dscms4.content',
@@ -20,4 +33,5 @@ setup(
         'dscms4.membership',
         'dscms4.menu',
         'dscms4.preview'],
-    description='HOMEINFO Digital Signage Content Management System v4.')
+    description='HOMEINFO Digital Signage Content Management System v4.'
+)
