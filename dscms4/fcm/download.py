e424314554aaf205ac7da4414102e3c0dafc3fcc
Richard Neumann <r.neumann@homeinfo.de>
2022-08-26 13:57:55 +0200

    Add Downloads messaging

---
+++ dscms4/fcm/download.py
@@ -0,0 +1,32 @@
+"""Downloads messaging."""
+
+from typing import Optional, Union
+
+from firebase_admin.messaging import BatchResponse
+
+from cmslib import GroupBaseChart
+from comcatlib import Menu
+from comcatlib import URLCode
+from comcatlib import UserBaseChart
+from comcatlib import get_tokens
+from comcatlib import multicast_chart
+
+from dscms4.fcm.common import affected_users, is_in_menu
+
+
+__all__ = ['notify']
+
+
+def notify(
+        target: Union[GroupBaseChart, UserBaseChart]
+) -> Optional[BatchResponse]:
+    """Notify via FCM about downloads."""
+
+    if not is_in_menu(target.base_chart, Menu.DOCUMENTS):
+        return
+
+    return multicast_chart(
+        target.chart,
+        URLCode.DOWNLOAD,
+        get_tokens(set(affected_users(target)))
+    )
