017efa72428ebe92bddd90af74f3cc5efc04e517
Richard Neumann <r.neumann@homeinfo.de>
2022-08-26 14:58:19 +0200

    Make notify_base_chart() return a list of notified users

--- dscms4/fcm/__init__.py
+++ dscms4/fcm/__init__.py
@@ -1,11 +1,7 @@
 """Firebase Cloud Messaging."""
 
-from typing import Optional
-
-from firebase_admin.messaging import BatchResponse
-
 from cmslib import BaseChart
-from comcatlib import Menu
+from comcatlib import Menu, User, evaluate_recipients
 
 from dscms4.fcm.common import is_in_menu
 from dscms4.fcm.download import notify as notify_download
@@ -15,13 +11,13 @@ from dscms4.fcm.news import notify as notify_news
 __all__ = ['notify_base_chart']
 
 
-def notify_base_chart(base_chart: BaseChart) -> Optional[BatchResponse]:
+def notify_base_chart(base_chart: BaseChart) -> list[User]:
     """Notify via FCM about base chart-related changes."""
 
     if is_in_menu(base_chart, Menu.DOCUMENTS):
-        return notify_download(base_chart)
+        return list(evaluate_recipients(notify_download(base_chart)))
 
     if is_in_menu(base_chart, Menu.NEWS):
-        return notify_news(base_chart)
+        return list(evaluate_recipients(notify_news(base_chart)))
 
-    return None
+    return []
