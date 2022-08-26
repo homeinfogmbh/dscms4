af7fec300fc3b652aefda34d72cf5f47bb241013
Richard Neumann <r.neumann@homeinfo.de>
2022-08-26 15:11:45 +0200

    Remove BatchResponse parsing

--- dscms4/fcm/__init__.py
+++ dscms4/fcm/__init__.py
@@ -1,7 +1,11 @@
 """Firebase Cloud Messaging."""
 
+from typing import Optional
+
+from firebase_admin.messaging import BatchResponse
+
 from cmslib import BaseChart
-from comcatlib import Menu, User, evaluate_recipients
+from comcatlib import Menu
 
 from dscms4.fcm.common import is_in_menu
 from dscms4.fcm.download import notify as notify_download
@@ -11,13 +15,13 @@ from dscms4.fcm.news import notify as notify_news
 __all__ = ['notify_base_chart']
 
 
-def notify_base_chart(base_chart: BaseChart) -> list[User]:
+def notify_base_chart(base_chart: BaseChart) -> Optional[BatchResponse]:
     """Notify via FCM about base chart-related changes."""
 
     if is_in_menu(base_chart, Menu.DOCUMENTS):
-        return list(evaluate_recipients(notify_download(base_chart)))
+        return notify_download(base_chart)
 
     if is_in_menu(base_chart, Menu.NEWS):
-        return list(evaluate_recipients(notify_news(base_chart)))
+        return notify_news(base_chart)
 
-    return []
+    return None
