9c7f3d2e38c51c8fb55eb0e24b36fb14c0c04552
Richard Neumann <r.neumann@homeinfo.de>
2022-08-29 10:55:37 +0200

    Update chart messaging API

--- dscms4/fcm/common.py
+++ dscms4/fcm/common.py
@@ -1,40 +1,13 @@
 """Common functions."""
 
-from typing import Iterable, Union
+from typing import Union
 
-from cmslib import BaseChart, GroupBaseChart
-from comcatlib import GroupMemberUser
+from cmslib import BaseChart
 from comcatlib import Menu
 from comcatlib import MenuBaseChart
-from comcatlib import User
-from comcatlib import UserBaseChart
-from comcatlib import expand_groups
 
 
-__all__ = ['affected_users', 'is_in_menu']
-
-
-def affected_users(
-        base_chart: Union[BaseChart, int]
-) -> Iterable[Union[User, int]]:
-    """Return a set of users affected by the
-    change to the respective chart mapping.
-    """
-
-    for user_base_chart in UserBaseChart.select().where(
-            UserBaseChart.base_chart == base_chart
-    ):
-        yield user_base_chart.user
-
-    for member in GroupMemberUser.select().where(
-            GroupMemberUser.group << expand_groups({
-                group_base_chart.group for
-                group_base_chart in GroupBaseChart.select().where(
-                    GroupBaseChart.base_chart == base_chart
-                )
-            })
-    ):
-        yield member.user
+__all__ = ['is_in_menu']
 
 
 def is_in_menu(base_chart: Union[BaseChart, int], menu: Menu) -> bool:
--- dscms4/fcm/news.py
+++ dscms4/fcm/news.py
@@ -4,10 +4,7 @@ from firebase_admin.messaging import BatchResponse
 
 from cmslib import BaseChart
 from comcatlib import URLCode
-from comcatlib import get_tokens
 from comcatlib import multicast_base_chart
-
-from dscms4.fcm.common import affected_users
 
 
 __all__ = ['notify']
@@ -16,8 +13,4 @@ __all__ = ['notify']
 def notify(base_chart: BaseChart) -> BatchResponse:
     """Notify via FCM about news."""
 
-    return multicast_base_chart(
-        base_chart.chart,
-        URLCode.NEWS,
-        get_tokens(set(affected_users(base_chart)))
-    )
+    return multicast_base_chart(base_chart.chart, URLCode.NEWS)
