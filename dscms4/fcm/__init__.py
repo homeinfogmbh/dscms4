a78883f7bfca00f84f9af09544d47ac25861da83
Richard Neumann <r.neumann@homeinfo.de>
2022-08-29 11:46:47 +0200

    Update FCM API

--- dscms4/fcm/__init__.py
+++ dscms4/fcm/__init__.py
@@ -4,15 +4,21 @@ from typing import Optional
 
 from firebase_admin.messaging import BatchResponse
 
+from ccmessenger import notify as notify_customer_message
 from cmslib import BaseChart
-from comcatlib import Menu, multicast_customer_event as notify_customer_event
+from comcatlib import Menu
+from tenantcalendar import notify as notify_customer_event
 
 from dscms4.fcm.common import is_in_menu
 from dscms4.fcm.download import notify as notify_download
 from dscms4.fcm.news import notify as notify_news
 
 
-__all__ = ['notify_base_chart', 'notify_customer_event']
+__all__ = [
+    'notify_base_chart',
+    'notify_customer_event',
+    'notify_customer_message'
+]
 
 
 def notify_base_chart(base_chart: BaseChart) -> Optional[BatchResponse]:
